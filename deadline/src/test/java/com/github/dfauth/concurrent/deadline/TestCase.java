a1d7f66c98579c9be90adfeb9bc3aa50e29c29d3
damian <dfauth@gmail.com>
2020-11-29 23:26:58 +0800

    tidy up deadline engine

--- deadline/src/test/java/com/github/dfauth/concurrent/deadline/TestCase.java
+++ deadline/src/test/java/com/github/dfauth/concurrent/deadline/TestCase.java
@@ -1,5 +1,6 @@
 package com.github.dfauth.concurrent.deadline;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,19 +16,24 @@ import static org.junit.Assert.*;
 public class TestCase {
 
     private static final Logger logger = LoggerFactory.getLogger(TestCase.class);
+    private Instant now;
+
+    @Before
+    public void setUp() {
+        now = Instant.now();
+    }
 
     @Test
     public void testScheduleCancel() {
         TestHandler handler = new TestHandler();
         DeadlineEngine engine = new DeadlineEngineImpl();
         assertEquals(0, engine.size());
-        Instant now = Instant.now();
-        Instant t = now.plus(Duration.ofSeconds(4));
-        long requestId = engine.schedule(t.toEpochMilli());
+        long t = secondsFromNow(4);
+        long requestId = engine.schedule(t);
         assertEquals(1, engine.size());
         engine.cancel(requestId);
         assertEquals(0, engine.size());
-        engine.poll(now.plus(Duration.ofSeconds(6)).toEpochMilli(), handler, 1);
+        engine.poll(secondsFromNow(6), handler, 1);
         assertFalse(handler.wasTriggered());
     }
 
@@ -36,15 +42,14 @@ public class TestCase {
         TestHandler handler = new TestHandler();
         DeadlineEngine engine = new DeadlineEngineImpl();
         assertEquals(0, engine.size());
-        Instant now = Instant.now();
-        long t = now.plus(Duration.ofSeconds(4)).toEpochMilli();
+        long t = secondsFromNow(4);
         long id = engine.schedule(t);
         assertEquals(engine.size(), 1);
         engine.poll(now.toEpochMilli(), handler, 1);
         assertFalse(handler.wasTriggered());
-        engine.poll(now.plus(Duration.ofSeconds(3)).toEpochMilli(), handler, 1);
+        engine.poll(secondsFromNow(3), handler, 1);
         assertFalse(handler.wasTriggered());
-        engine.poll(now.plus(Duration.ofSeconds(6)).toEpochMilli(), handler, 1);
+        engine.poll(secondsFromNow(6), handler, 1);
         assertTrue(handler.wasTriggered());
         assertTrue(handler.wasTriggeredWith(id));
     }
@@ -54,20 +59,19 @@ public class TestCase {
         TestHandler handler = new TestHandler();
         DeadlineEngine engine = new DeadlineEngineImpl();
         assertEquals(0, engine.size());
-        Instant now = Instant.now();
         long id1 = engine.schedule(now.plus(Duration.ofSeconds(4)).toEpochMilli());
         assertEquals(1, engine.size());
         long id2 = engine.schedule(now.plus(Duration.ofSeconds(6)).toEpochMilli());
         assertEquals(2, engine.size());
         engine.poll(now.toEpochMilli(), handler, 1);
         assertFalse(handler.wasTriggered());
-        engine.poll(now.plus(Duration.ofSeconds(3)).toEpochMilli(), handler, 1);
+        engine.poll(secondsFromNow(3), handler, 1);
         assertFalse(handler.wasTriggered());
-        engine.poll(now.plus(Duration.ofSeconds(5)).toEpochMilli(), handler, 1);
+        engine.poll(secondsFromNow(5), handler, 1);
         assertTrue(handler.wasTriggered());
         assertTrue(handler.wasTriggeredWith(id1));
         assertFalse(handler.wasTriggeredWith(id2));
-        engine.poll(now.plus(Duration.ofSeconds(7)).toEpochMilli(), handler, 1);
+        engine.poll(secondsFromNow(7), handler, 1);
         assertTrue(handler.wasTriggered());
         assertTrue(handler.wasTriggeredWith(id1));
         assertTrue(handler.wasTriggeredWith(id2));
@@ -84,7 +88,7 @@ public class TestCase {
         assertEquals(1, engine.size());
         long id2 = engine.schedule(t);
         assertEquals(2, engine.size());
-        long t1 = now.plus(Duration.ofSeconds(1)).toEpochMilli();
+        long t1 = secondsFromNow(1);
         engine.poll(t1, handler, 1);
         assertEquals(1, engine.size());
         assertTrue(handler.wasTriggered());
@@ -136,4 +140,13 @@ public class TestCase {
             return requestIds;
         }
     }
+
+    private long secondsFromNow(long seconds) {
+        return addSeconds(now, seconds);
+    }
+
+    private long addSeconds(Instant now, long seconds) {
+        return now.plus(Duration.ofSeconds(seconds)).toEpochMilli();
+    }
+
 }
